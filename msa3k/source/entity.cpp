8d223a0b20c63ce2c4ed28a158d361289da8105a
Jonathan Tao <jon.g.tao@gmail.com>
2013-08-24 19:01:16 -0700

    Added abstract entity class

---
+++ msa3k/source/entity.cpp
@@ -0,0 +1,13 @@
+#include "entity.h"
+
+Entity::Entity(int i): id_num(i),
+	x(0), y(0), vx(0), vy(0)
+{
+} // Entity()
+
+
+
+int Entity::fetch_id()
+{
+	return id_num;
+} // fetch_id()
---
+++ msa3k/source/entity.h
@@ -0,0 +1,31 @@
+// Abstract Entity class
+//
+// Contains data for entity
+
+#ifndef ENTITY_H
+#define ENTITY_H
+
+//#include "input.h"
+//#include "mapdata.h"
+
+class Entity
+{
+private:
+	int id_num;
+	int x, y; // position
+	int vx, vy; // velocity
+	int top, bottom, left, right; // body box
+	//Input inputs;
+	//MapData *currentMap;
+
+public:
+	Entity(int i);
+	~Entity();
+
+	int fetch_id();
+
+	virtual void update() = 0;
+	friend class Render;
+}; // class Entity
+
+#endif
